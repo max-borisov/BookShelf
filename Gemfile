32bce111c669d131452cf4a6e5a00f83c1969628
max-borisov <max.borisov@yahoo.com>
2015-01-07 16:51:13 +0200

    Add User model

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'spring',        group: :development
 gem 'mysql2', '~> 0.3.17'
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.9'
 
 # Use unicorn as the app server
 # gem 'unicorn'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106091236) do
+ActiveRecord::Schema.define(version: 20150106205927) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20150106091236) do
     t.datetime "updated_at"
     t.string   "amazon_id"
     t.text     "publisher"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
