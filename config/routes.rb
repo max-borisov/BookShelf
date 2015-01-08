09640cda407b46db139c9bccd9b7a8d95b79f07e
max-borisov <max.borisov@yahoo.com>
2015-01-08 11:19:35 +0200

    Add form footer partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root to: "books#index"
+  get   'signup'  => 'users#new'
+  get   'signin'  => 'sessions#new'
+  post  'signin'  => 'sessions#create'
   resources :books
   resources :users
-
-  root to: "books#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
