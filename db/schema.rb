cb04b3ee0f71d17576bb156bb9c450b5b9a2dc10
max-borisov <max.borisov@yahoo.com>
2015-01-09 09:06:20 +0200

    Remember me option

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106205927) do
+ActiveRecord::Schema.define(version: 20150109051049) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150106205927) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_digest"
   end
 
 end
