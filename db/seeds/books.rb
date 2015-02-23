1ec8b69b90c0e74d6b47d01c1f9098edf1425ace
max-borisov <max.borisov@yahoo.com>
2015-02-23 13:22:08 +0200

    Add new books to seeds files. Update catalog and book view.

--- db/seeds/books.rb
+++ db/seeds/books.rb
@@ -1,36 +1,222 @@
 Book.create!([
   {
       id: 1,
-      title: 'Ruby',
-      author: 'Ruby author',
-      pub_date: '12/12/2014',
-      description: 'Cool book',
-      price: 12.12,
-      isbn: 111223331,
+      title: 'Rails 4 Test Prescriptions: Build a Healthy Codebase',
+      author: 'Noel Rappin',
+      pub_date: '13/12/2014',
+      description: "Does your Rails code suffer from bloat, brittleness, or inaccuracy? Cure these problems with a regular dose of test-driven development. Rails 4 Test Prescriptions is a comprehensive guide to how tests can help you design and write better Rails applications. In this completely revised edition, you'll learn why testing works and how to test effectively using Rails 4, Minitest 5, and RSpec 3, as well as popular testing libraries such as factory_girl and Cucumber. Do what the doctor ordered to make your applications feel all better. Side effects may include better code, fewer bugs, and happier developers.
+
+Your Ruby on Rails application is sick. Deadlines are looming, but every time you make the slightest change to the code, something else breaks. Nobody remembers what that tricky piece of code was supposed to do, and nobody can tell what it actually does. Plus, it has bugs. You need test-driven development, a process for improving the design, maintainability, and long-term viability of software.
+
+Containing both practical code examples and discussion of why testing works, this book starts with the most basic features delivered as part of core Ruby on Rails. Once you've integrated those features into your coding practice, you'll learn how to use popular third-party testing tools such as RSpec, Mocha, Cucumber, and factory_girl. You'll test the component parts of a Rails application, including the back-end model logic and the front-end display logic. Using Rails examples, you'll learn how to use testing to enable your code to respond better to future change. Plus, you'll see how to handle real-world testing situations.
+
+This completely revised edition contains a new tutorial, as well as new examples throughout the book. Many chapters, including the JavaScript chapter, have undergone major changes to reflect new tools and new practices. And there are brand new chapters on testing for security, and testing external services.
+
+What You Need:Ruby 2.1, Rails 4",
+      price: 34.34,
+      isbn: 1941222196,
       amazon_id: '1941222196',
       publisher: 'Pragmatic Bookshelf',
   },
   {
       id: 2,
-      title: 'PHP',
-      author: 'PHP author',
-      pub_date: '12/12/2014',
-      description: 'Cool book',
-      price: 12.12,
-      isbn: 111223331,
-      amazon_id: '1449392776',
-      publisher: 'Addison-Wesley Professional',
+      title: 'Web Application Development with Yii 2 and PHP',
+      author: 'Mark Safronov, Jeffrey Winesett',
+      pub_date: '25/09/2014',
+      description: "Fast-track your web application development using the new generation Yii PHP framework
+
+About This Book
+Implement real-world web application features efficiently using the Yii development framework
+Each chapter provides micro-examples that build upon each other to create the final macro-example, a basic CRM application
+Filled with useful tasks to improve the maintainability of your applications
+Who This Book Is For
+
+This book is for professional PHP developers who wish to master the powerful Yii 2 application framework. It is assumed that you have knowledge of object-oriented programming. The previous version of the Yii framework is only briefly mentioned, but it'll be even easier to grasp Yii 2 with the knowledge of Yii 1.1.x.
+
+      In Detail
+Yii is a high performance PHP framework used for rapid web application development. It is well designed, well supported, easy to learn, and easy to maintain. This book embraces the learn-by-example methodology to show you the most important features of the Yii 2 framework. Throughout the course of this book, you will build a simple real-world application; each chapter will introduce you to a new functionality and show you how to tweak your application. Instead of trying to be an all-encompassing reference about the framework, this is a walkthrough of the really important pieces of information that you have to understand in detail.
+
+You will learn how to use Yii's active record and CRUD scaffolding to manage the data in your database. Authentication, extensions, events and behaviors, and route management are just some of the many other features of Yii that you will learn from this book. By the end of this book, you will have a basic CRM application that is all set for service!",
+      price: 32.39,
+      isbn: 1783981881,
+      amazon_id: '1783981881',
+      publisher: 'Packt Publishing',
   },
   {
       id: 3,
-      title: 'NodeJS',
-      author: 'NodeJS author',
-      pub_date: '12/12/2014',
-      description: 'Cool book',
-      price: 12.12,
-      isbn: 111223331,
-      amazon_id: '1449333605',
+      title: 'Metaprogramming Ruby',
+      author: 'Paolo Perrotta',
+      pub_date: '25/02/2010',
+      description: "Everyone in the Ruby world seems to be talking about metaprogramming--how you can use it to remove duplication in your code and write elegant, beautiful programs. Now you can get in on the action as well.
+
+This book describes metaprogramming as an essential component of Ruby. Once you understand the principles of Ruby, including the object model, scopes, and eigenclasses, you're on your way to applying metaprogramming both in your daily work and in your fun, after-hours projects.
+
+    Learning metaprogramming doesn't have to be difficult or boring. By taking you on a Monday-through-Friday workweek adventure with a pair of programmers, Paolo Perrotta helps make mastering the art of metaprogramming both straightforward and entertaining.
+
+The book is packed with:
+
+
+Pragmatic examples of metaprogramming in action, many of which come straight from popular libraries or frameworks, such as Rails.
+Programming challenges that let you experiment and play with some of the most fun, \"out-there\" metaprogramming concepts.
+Metaprogramming spells--34 practical recipes and idioms that you can study and apply right now, to write code that is sure to impress.
+
+Whether you're a Ruby apprentice on the path to mastering the language or a Ruby wiz in search of new tips, this book is for you.",
+      price: 23.07,
+      isbn: 1934356476,
+      amazon_id: '1934356476',
+      publisher: 'Pragmatic Bookshelf',
+  },
+  {
+      id: 4,
+      title: 'Refactoring: Ruby Edition',
+      author: 'Jay Fields, Shane Harvie, Martin Fowler, Kent Beck',
+      pub_date: '28/11/2013',
+      description: 'The Definitive Refactoring Guide, Fully Revamped for Ruby With refactoring, programmers can transform even the most chaotic software into well-designed systems that are far easier to evolve and maintain. What’s more, they can do it one step at a time, through a series of simple, proven steps. Now, there’s an authoritative and extensively updated version of Martin Fowler’s classic refactoring book that utilizes Ruby examples and idioms throughout–not code adapted from Java or any other environment.
+
+The authors introduce a detailed catalog of more than 70 proven Ruby refactorings, with specific guidance on when to apply each of them, step-by-step instructions for using them, and example code illustrating how they work. Many of the authors’ refactorings use powerful Ruby-specific features, and all code samples are available for download.
+
+Leveraging Fowler’s original concepts, the authors show how to perform refactoring in a controlled, efficient, incremental manner, so you methodically improve your code’s structure without introducing new bugs. Whatever your role in writing or maintaining Ruby code, this book will be an indispensable resource.
+
+This book will help you
+•    Understand the core principles of refactoring and the reasons for doing it
+•    Recognize “bad smells” in your Ruby code
+•    Rework bad designs into well-designed code, one step at a time
+•    Build tests to make sure your refactorings work properly
+•    Understand the challenges of refactoring and how they can be overcome
+•    Compose methods to package code properly
+•    Move features between objects to place responsibilities where they fit best
+•    Organize data to make it easier to work with
+•    Simplify conditional expressions and make more effective use of polymorphism
+•    Create interfaces that are easier to understand and use
+•    Generalize more effectively
+•    Perform larger refactorings that transform entire software systems and may take months or years
+•    Successfully refactor Ruby on Rails code',
+      price: 49.68,
+      isbn: '0321984137',
+      amazon_id: '0321984137',
+      publisher: 'Addison-Wesley',
+  },
+  {
+      id: 5,
+      title: 'Agile Web Development with Rails 4',
+      author: 'Sam Ruby, Dave Thomas, David Heinemeier Hansson',
+      pub_date: '11/10/2013',
+      description: "Rails just keeps on changing. Both Rails 3 and 4, as well as Ruby 1.9 and 2.0, bring hundreds of improvements, including new APIs and substantial performance enhancements. The fourth edition of this award-winning classic has been reorganized and refocused so it's more useful than ever before for developers new to Ruby and Rails.
+      Rails 4 introduces a number of user-facing changes, and the ebook has been updated to match all the latest changes and new best practices in Rails. This includes full support for Ruby 2.0, controller concerns, Russian Doll caching, strong parameters, Turbolinks, new test and bin directory layouts, and much more.
+      Ruby on Rails helps you produce high-quality, beautiful-looking web applications quickly. You concentrate on creating the application, and Rails takes care of the details.
+      Tens of thousands of developers have used this award-winning book to learn Rails. It's a broad, far-reaching tutorial and reference that's recommended by the Rails core team. If you're new to Rails, you'll get step-by-step guidance. If you're an experienced developer, this book will give you the comprehensive, insider information you need.
+Rails has evolved over the years, and this book has evolved along with it. We still start with a step-by-step walkthrough of building a real application, and in-depth chapters look at the built-in Rails features. This edition now gives new Ruby and Rails users more information on the Ruby language and takes more time to explain key concepts throughout. Best practices on how to apply Rails continue to change, and this edition keeps up. Examples use Concerns, Russian Doll caching, and Turbolinks, and the book focuses throughout on the right way to use Rails. Additionally, this edition now works on Ruby 2.0, a new release of Ruby with substantial functional and performance improvements.
+This edition is for Rails4.0 and beyond.",
+      price: 29.74,
+      isbn: '1937785564',
+      amazon_id: '1937785564',
+      publisher: 'Pragmatic Bookshelf',
+  },
+  {
+      id: 6,
+      title: 'Ruby Cookbook',
+      author: 'Lucas Carlson, Leonard Richardson',
+      pub_date: '23/08/2015',
+      description: "Push Ruby to its limits. Ruby Cookbook is the most comprehensive problem-solving guide to this popular language, with hundreds of solutions to real-world problems. Each detailed recipe provides code that you can use in your own projects right away, as well as a discussion on why the solution works.
+Updated to include Ruby 1.9 and 2.0, the second edition includes recipes for the Rails framework and other projects involving Ruby. From data structures and algorithms to integration with cutting-edge technologies, Ruby Cookbook covers a wide range of programming topics for beginners and advanced Rubyists alike.",
+      price: 34.35,
+      isbn: '1449373712',
+      amazon_id: '1449373712',
+      publisher: 'O\'Reilly Media',
+  },
+  {
+      id: 7,
+      title: 'Beginning Ruby: From Novice to Professional',
+      author: 'Peter Cooper',
+      pub_date: '19/07/2009',
+      description: "Based on the bestselling first edition, Beginning Ruby: From Novice to Professional, Second Edition is the leading guide for every type of reader who wants to learn Ruby from the ground up.
+
+The new edition of this book provides the same excellent introduction to Ruby as the first edition plus updates for the newest version of Ruby, including the addition of the Sinatra and Ramaze web application frameworks and a chapter on GUI development so developers can take advantage of these new trends.
+
+Beginning Ruby starts by explaining the principles behind object-oriented programming and within a few chapters builds toward creating a full Ruby application. By the end of the book, in addition to in-depth knowledge of Ruby, you'll also have basic understanding of many ancillary technologies such as SQL, XML, web frameworks, and networking.
+
+Introduces readers to the Ruby programming language
+Takes readers from basic programming skills to web development with topics like Ruby-based frameworks and GUI programming
+Covers many ancillary technologies in order to provide a broader picture (e.g., databases, XML, network daemons)
+What you’ll learn
+Understand the basics of Ruby and object-oriented building blocks.
+Work with Ruby libraries, gems, and documentation.
+Work with files and databases.
+Write and deploy Ruby applications.
+Explore Ruby web frameworks and aspects of network programming with Ruby.
+Develop desktop and GUI applications with Ruby.
+Who this book is for
+Beginning programmers, programmers new to Ruby, and web developers interested in knowing the foundations of the language.",
+      price: 27.78,
+      isbn: '1430223634',
+      amazon_id: '1430223634',
+      publisher: 'Apress',
+  },
+  {
+      id: 8,
+      title: 'Rails 4 in Action',
+      author: 'Ryan Bigg, Yehuda Katz, Steve Klabnik',
+      pub_date: '31/03/2015',
+      description: "Ruby on Rails is an open source web framework. It's famously simple to build a basic Rails application. Once you've started down the track, however, you discover that Rails is a powerful, deep, and rich full-stack platform capable of tackling virtually any development challenge.
+
+Rails 4 in Action is a fully-revised second edition of Rails 3 in Action. This hands-on, comprehensive introduction to Rails gives readers everything they need to become competent and confident Rails developers. It shows how to develop a ticket tracking application that includes RESTful routing, authentication and authorization, file uploads, email, and more. It explores powerful features like designing APIs and building a Rails engine. The book also provides Test Driven Development and Behavior Driven Development in action throughout the book—just like in a top Rails shop.
+
+Purchase of the print book includes a free eBook in PDF, Kindle, and ePub formats from Manning Publications.",
+      price: 40.41,
+      isbn: '1617291099',
+      amazon_id: '1617291099',
       publisher: 'Manning Publications',
   },
+  {
+      id: 9,
+      title: 'The Well-Grounded Rubyist',
+      author: 'David A. Black',
+      pub_date: '04/07/2014',
+      description: "The Well-Grounded Rubyist, Second Edition addresses both newcomers to Ruby as well as Ruby programmers who want to deepen their understanding of the language. This beautifully written and totally revised second edition includes coverage of features that are new in Ruby 2.1, as well as expanded and updated coverage of aspects of the language that have changed.
+
+Purchase of the print book includes a free eBook in PDF, Kindle, and ePub formats from Manning Publications.
+
+About the Technology
+
+This is a good time for Ruby! It's powerful like Java or C++, and has dynamic features that let your code react gracefully to changes at runtime. And it's elegant, so creating applications, development tools, and administrative scripts is easier and more straightforward. With the long-awaited Ruby 2, an active development community, and countless libraries and productivity tools, Ruby has come into its own.
+
+About the Book
+
+The Well-Grounded Rubyist, Second Edition is a beautifully written tutorial that begins with your first Ruby program and goes on to explore sophisticated topics like callable objects, reflection, and threading. The book concentrates on the language, preparing you to use Ruby in any way you choose. This second edition includes coverage of new Ruby features such as keyword arguments, lazy enumerators, and Module#prepend, along with updated information on new and changed core classes and methods.
+
+What's Inside
+
+Clear explanations of Ruby concepts
+Numerous simple examples
+Updated for Ruby 2.1
+Prepares you to use Ruby anywhere for any purpose
+About the Author
+
+David A. Black is an internationally known Ruby developer, author, trainer, speaker, event organizer, and founder of Ruby Central, as well as a Lead Consultant at Cyrus Innovation.",
+      price: 32.10,
+      isbn: '1617291692',
+      amazon_id: '1617291692',
+      publisher: 'Manning Publications',
+  },
+  {
+      id: 10,
+      title: 'The Little Book on CoffeeScript',
+      author: 'Alex MacCaw',
+      pub_date: '31/01/2012',
+      description: "This concise book shows JavaScript developers how to build superb web applications with CoffeeScript, the remarkable language that’s gaining considerable interest. Through example code, this guide demonstrates how CoffeeScript abstracts JavaScript, providing syntactical sugar and preventing many common errors. You’ll learn CoffeeScript’s syntax and idioms step by step, from basic variables and functions to complex comprehensions and classes.
+
+Written by Alex MacCaw, author of JavaScript Web Applications (O’Reilly), with contributions from CoffeeScript creator Jeremy Ashkenas, this book quickly teaches you best practices for using this language—not just on the client side, but for server-side applications as well. It’s time to take a ride with the little language that could.
+
+Discover how CoffeeScript’s syntax differs from JavaScript
+Learn about features such as array comprehensions, destructuring assignments, and classes
+Explore CoffeeScript idioms and compare them to their JavaScript counterparts
+Compile CoffeeScript files in static sites with the Cake build system
+Use CommonJS modules to structure and deploy CoffeeScript client-side applications
+Examine JavaScript’s bad parts—including features CoffeeScript was able to fix.",
+      price: 8.99,
+      isbn: '1449321054',
+      amazon_id: '1449321054',
+      publisher: 'O\'Reilly Media',
+  }
 ])
 puts("#{Book.count} book(s) have been created.")
