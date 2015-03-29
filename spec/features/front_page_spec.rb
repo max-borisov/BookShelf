5f1f038e6340a8b07432e8872897ec706013ebb6
max-borisov <max.borisov@yahoo.com>
2015-03-29 17:24:57 +0300

    Update feature test for the front page.

--- spec/features/front_page_spec.rb
+++ spec/features/front_page_spec.rb
@@ -1,29 +1,27 @@
 require 'rails_helper'
 
 describe "front page", :type => :feature do
-  before(:each) do
+  before(:all) do
     create(:book, title: 'ruby')
     create(:book, title: 'ruby on rails')
     create(:book, title: 'php')
+    create_list(:book, 13)
   end
 
-  # let!(:book) { create(:book, title: 'ruby') }
+  before(:each) do
+    visit '/'
+  end
 
   it "has page title and content" do
-=begin
-    create(:book, title: 'ruby')
-    create(:book, title: 'ruby on rails')
-    create(:book, title: 'php')
-=end
-
-
-
-    visit '/'
-
     expect(page).to have_content('Pragmatic bookshelf')
     expect(page).to have_selector('div.book-info-block')
+  end
 
-    # save_and_open_screenshot
+  context 'when click on book\'s title' do
+    it 'opens book\'s page' do
+      all('.book-info-block')[0].all('a')[0].click
+      expect(page).to have_selector('h1')
+    end
   end
 
   context 'when click on sign in link' do
@@ -44,18 +42,18 @@ describe "front page", :type => :feature do
   end
 
   describe 'search books form' do
-    context 'when search for "rails" text' do
-      xit 'shows list of books which contain "rails" keyword' do
+    context 'when matches are found' do
+      it 'shows 2 books which contain "ruby" keyword' do
         within('.search-book-form') do
-          fill_in 'keywords', :with => 'rails'
+          fill_in 'keywords', :with => 'ruby'
           click_on('Search')
         end
-        expect(page).to have_selector('div.book-info-block')
+        expect(all('div.book-info-block').length).to eq(2)
       end
     end
 
-    context 'when search for "oops" text' do
-      xit 'shows a message that books were not found' do
+    context 'when matches were not found' do
+      it 'shows info message when after attempt to search book with "oops" keyword' do
         within('.search-book-form') do
           fill_in 'keywords', :with => 'oops'
           click_on('Search')
@@ -64,10 +62,14 @@ describe "front page", :type => :feature do
       end
     end
 
-    describe 'Reset form button' do
-      xit 'reloads the page and resets search form' do
-        click_link('Reset')
-        expect(find_field('keywords').value).to eq('')
+    describe 'reset form' do
+      context 'when click on Reset button' do
+        it 'reloads the page and resets search form' do
+          within('.search-book-form') do
+            click_on('Reset')
+          end
+          expect(find_field('keywords').value).to eq(nil)
+        end
       end
     end
   end
