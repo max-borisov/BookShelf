10c051a80e5535285c69a70faaa2ab47ec1e59cc
max-borisov <max.borisov@yahoo.com>
2015-04-14 09:41:24 +0300

    Update feature tests for shopping cart.

--- spec/features/shopping_cart_page_spec.rb
+++ spec/features/shopping_cart_page_spec.rb
@@ -9,6 +9,7 @@ describe "shopping cart page", :type => :feature do
   end
 
   context 'when user is logged in' do
+    # @todo use let() instead. all vs each vs let. what is better ?
     before(:all) do
       @tom = create(:user, email: 'tom@gmail.com')
       @bob = create(:user, email: 'bob@gmail.com')
@@ -37,13 +38,51 @@ describe "shopping cart page", :type => :feature do
       end
 
       it 'has list of saved books' do
-        # save_and_open_page
         expect(all('.book-cover-mini').count).to eq(3)
       end
 
-      xit 'shows total price'
-      xit 'has "Checkout" button'
-      xit 'has "Delete from cart" button'
+      it 'shows total price' do
+        expect(find('.cart-items-info-message')).to have_content('Total price: $' + 30.to_s)
+      end
+
+      it 'has "Checkout" button' do
+        expect(find('.cart-items-info-message')).to have_selector('input[value="Checkout"]')
+      end
+
+      it 'has "Delete from cart" button' do
+        expect(all('input[value="Delete from cart"]').count).to eq(3)
+      end
+
+      context 'when delete book from cart' do
+        before(:each) do
+          accept_alert do
+            first('form input[value="Delete from cart"]').click
+          end
+        end
+
+        it 'shows notification message and desreases books amout', js: true do
+          expect(page).to have_content('has been removed for the shopping cart')
+          expect(all('form input[value="Delete from cart"]').count).to eq(2)
+        end
+      end
+
+      context 'when checkout' do
+        before(:each) do
+          click_button('Checkout')
+        end
+
+        it 'gets redirected to the orders page' do
+          expect(page).to have_content('Orders history')
+        end
+
+        it 'shows how many books were ordered' do
+          expect(page).to have_content('have been purchased')
+        end
+
+        it 'shows ordered books' do
+          expect(page).to have_selector('.order-history-row')
+        end
+      end
     end
   end
 end
